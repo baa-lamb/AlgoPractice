7cbed122fa7ced7034202ad5b8c6ce09c422e985
baa-lamb <alena.vasileva.2017@yandex.ru>
2022-01-28 17:24:46 +0300

    первая задача на codeforces

--- A4/Source.cpp
+++ A4/Source.cpp
@@ -4,12 +4,18 @@ using namespace std;
 int main() {
 	int w = 0;
 	cin >> w;
-	if (w % 2 == 0) {
-		cout << "YES";
-	}
-	else {
+	if (w == 2) {
 		cout << "NO";
+		return 0;
 	}
+	for (int i = 2; i < w; i+=2) {
+		if ((w - i) % 2 == 0) {
+			cout << "YES";
+			return 0;
+		}
+	}
+	cout << "NO";
+	
 
 	return 0;
 }
